df1ab13ea9bbed59330ad4e30582cca6242f8bf4
AlexanderAuton <350319867@tdsb.ca>
2026-02-12 16:29:44 +0000

    Error404:NameNotFound

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,9 @@
+/**
+* File Name: Lesson 1.3 - Strings
+* Author: Mr. Auton
+* Date Created: Feb 12, 2026
+* Date Last Modified: Feb 12, 2026
+*/
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +15,49 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n" + //
+						"* * * * * *\n" + //
+						" * * * * * *\n" + //
+						"* * * * * *\n" + //
+						" * * * * * *\n" + //
+						"* * * * * *\n");
+		System.out.println("* * * * *\n" + //
+						"*       *\n" + //
+						"*       *\n" + //
+						"*       *\n" + //
+						"* * * * *\n");
+		System.out.println("* * * * *\n" + //
+						" *       *\n" + //
+						"  *     *\n" + //
+						"   *   *\n" + //
+						"    * *\n" + //
+						"     *");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		String dog = "          '__'\n" + //
+						"          (oo)\n" + //
+						"  /========//\n" + //
+						" / || XX ||\n" + //
+						"*  ||----||\n" + //
+						"   VV    VV\n" + //
+						"   ''    ''";
+						System.out.println(dog);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String A = "Fortnite";
+		String B = "is awful";
+		System.out.println(A + " " + B);
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		String C = "Don't";
+		String D = "@ me";
+		System.out.println(C + " " + D);
 	}
 
 
